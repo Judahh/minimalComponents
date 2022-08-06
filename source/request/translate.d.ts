faf190247215b37f7248b57ea7a998a2ef7b4d1e
Judah <judahholanda7@gmail.com>
2022-08-06 15:47:13 -0300

    add translate

---
+++ source/request/translate.d.ts
@@ -0,0 +1,3 @@
+declare const translateText: (key: string, text: string, targetLanguage?: string) => Promise<string>;
+export { translateText };
+//# sourceMappingURL=translate.d.ts.map
---
+++ source/request/translate.ts
@@ -0,0 +1,47 @@
+// const { Translate } = require('@google-cloud/translate').v2;
+// const translate = new Translate();
+import { request } from '.';
+
+const escapeRegExp = (text: string) => {
+  return text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+};
+
+const encodeText = (text: string) => {
+  return text
+    .replace(new RegExp(escapeRegExp(' '), 'g'), '%20')
+    .replace(new RegExp(escapeRegExp('\n'), 'g'), '$UR$');
+};
+
+const decodeText = (text: string) => {
+  return text
+    .replace(new RegExp(escapeRegExp('%20'), 'g'), ' ')
+    .replace(new RegExp(escapeRegExp('$ UR $'), 'g'), '\n')
+    .replace(new RegExp(escapeRegExp('$UR$'), 'g'), '\n');
+};
+
+const translateText = async (key: string, text: string, targetLanguage?: string) => {
+  if (
+    targetLanguage === undefined ||
+    targetLanguage === null ||
+    targetLanguage?.toLowerCase()?.includes('en')
+  )
+    return text;
+
+  const data = await request(
+    `https://translation.googleapis.com/language/translate/v2?key=${key}`,
+    'get',
+    `&q=${encodeText(text)}&target=${targetLanguage}&source=en`,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    {}
+  );
+
+  text = decodeText(data?.data?.data?.translations[0]?.translatedText) || text;
+  return text;
+};
+
+export { translateText };
