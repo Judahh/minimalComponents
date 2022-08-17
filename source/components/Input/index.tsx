ee6a967c8f7c96f52fedc86ec6151ea3cc75d1a1
Judah <judahholanda7@gmail.com>
2022-08-17 15:47:58 -0300

    improve inputs events

--- source/components/Input/index.tsx
+++ source/components/Input/index.tsx
@@ -13,39 +13,102 @@ const Input = (props: {
   iconValue?: boolean;
   value?;
   setValue?;
-  onChange?;
-  onClick?;
-  onInput?;
-  onSubmit?;
-  onKeyUp?;
-  onKeyDown?;
+  onChange?: (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void
+  ) => void;
+  onClick?: (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void
+  ) => void;
+  onInput?: (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void
+  ) => void;
+  onSubmit?: (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void
+  ) => void;
+  onKeyUp?: (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void
+  ) => void;
+  onKeyDown?: (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void
+  ) => void;
   label?: string;
   labelStyle?: CSSProperties;
   labelParentStyle?: CSSProperties;
-  validate?: (value?, valueState?: [any, (error?) => void], error?, setError?: (error?) => void) => void;
+  stopPropagation?: boolean;
+  validate?: (
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?) => void,
+    event?,
+    eventF?
+  ) => void;
 }) => {
   const inputRef = useRef<HTMLButtonElement>(null);
-  const valueState: [any, (error?) => void] = props?.setValue ? [props?.value, props?.setValue ] : useState<any | undefined>(props.defaultValue || props.value);
-  const errorState = props?.setError ? undefined : useState<any | undefined>(props.defaultError || props.error);
+  const valueState: [any, (error?) => void] = props?.setValue
+    ? [props?.value, props?.setValue]
+    : useState<any | undefined>(props.defaultValue || props.value);
+  const errorState = props?.setError
+    ? undefined
+    : useState<any | undefined>(props.defaultError || props.error);
 
-  const basicValidate = (value?, valueState?: [any, (error?) => void], error?, setError?: (error?:string) => void) => {
+  const basicValidate = (
+    event?,
+    value?,
+    valueState?: [any, (error?) => void],
+    error?,
+    setError?: (error?: string) => void,
+    eventF?
+  ) => {
     // console.log('basicValidate', value, valueState);
-    valueState?.[1]?.(value);
-    setError?.(error);
-  }
+    if (props?.stopPropagation) event?.stopPropagation();
+    if (props?.validate)
+      props.validate(value, valueState, error, setError, event, eventF);
+    else {
+      valueState?.[1]?.(value);
+      setError?.(error);
+    }
+    eventF?.(event, value, valueState, error, setError);
+  };
 
+  useEffect(() => {}, [props?.error]);
 
   useEffect(() => {
-  }, [props?.error]);
-
-  useEffect(() => {
-    if(!props?.setValue)
-      if(props?.validate)
-        props.validate?.(props?.value, props?.setValue, props?.error, props?.setError);
+    if (!props?.setValue)
+      if (props?.validate)
+        props.validate?.(
+          props?.value,
+          props?.setValue,
+          props?.error,
+          props?.setError
+        );
   }, [props?.value]);
 
-  useEffect(() => {
-  }, [props, Object.values(props)]);
+  useEffect(() => {}, [props, Object.values(props)]);
 
   const getProps = () => {
     const newProps = JSON.parse(JSON.stringify(props));
@@ -58,58 +121,109 @@ const Input = (props: {
     delete newProps.labelParentStyle;
     delete newProps.errorStyle;
     newProps.validate = props.validate;
-    newProps.onClick = props.onClick;
-    newProps.onChange = props.onChange;
-    newProps.onInput= props.onInput;
-    newProps.onSubmit= props.onSubmit;
-    newProps.onKeyUp= props.onKeyUp;
-    newProps.onKeyDown= props.onKeyDown;
 
     newProps.defaultValue = props?.defaultValue || props?.value;
     newProps.value = valueState?.[0];
-    newProps.onChange = (event) => props?.validate
-      ? props.validate?.(event.target.value, valueState, props?.error || errorState?.[0], props?.setError || errorState?.[1])
-      : basicValidate(event.target.value, valueState, props?.error || errorState?.[0], props?.setError || errorState?.[1]);
+    newProps.onChange = (event) =>
+      basicValidate(
+        event,
+        event.target.value,
+        valueState,
+        props?.error || errorState?.[0],
+        props?.setError || errorState?.[1],
+        props.onChange
+      );
+    newProps.onClick = (event) => {
+      if (props?.stopPropagation) event?.stopPropagation();
+      return props?.onClick?.(
+        event,
+        event.target.value,
+        valueState,
+        props?.error || errorState?.[0],
+        props?.setError || errorState?.[1]
+      );
+    };
+    newProps.onInput = (event) => {
+      if (props?.stopPropagation) event?.stopPropagation();
+      return props?.onInput?.(
+        event,
+        event.target.value,
+        valueState,
+        props?.error || errorState?.[0],
+        props?.setError || errorState?.[1]
+      );
+    };
+    newProps.onSubmit = (event) => {
+      if (props?.stopPropagation) event?.stopPropagation();
+      return props?.onSubmit?.(
+        event,
+        event.target.value,
+        valueState,
+        props?.error || errorState?.[0],
+        props?.setError || errorState?.[1]
+      );
+    };
+    newProps.onKeyUp = (event) => {
+      if (props?.stopPropagation) event?.stopPropagation();
+      return props?.onKeyUp?.(
+        event,
+        event.target.value,
+        valueState,
+        props?.error || errorState?.[0],
+        props?.setError || errorState?.[1]
+      );
+    };
+    newProps.onKeyDown = (event) => {
+      if (props?.stopPropagation) event?.stopPropagation();
+      return props?.onKeyDown?.(
+        event,
+        event.target.value,
+        valueState,
+        props?.error || errorState?.[0],
+        props?.setError || errorState?.[1]
+      );
+    };
 
-    if(newProps.value != undefined)
-      delete newProps.defaultValue;
+    if (newProps.value != undefined) delete newProps.defaultValue;
     return newProps;
-  }
+  };
 
-  const input = props.type === 'file'?
-  (<>
-    <InputStyle
-      {...{...getProps(), value:''}}
-      style={{display:'none'}}
-      ref={inputRef}
-    />
-    <InputStyle
-      {...{...getProps(), type:'button'}}
-      onClick={()=>{
-        // console.log('CLICK');
-        // console.log('click', inputRef?.current);
-        inputRef?.current?.click()
-      }}
-    />
-  </>):
-  (<InputStyle
-    {...getProps()}
-    ref={inputRef}
-  />);
+  const input =
+    props.type === 'file' ? (
+      <>
+        <InputStyle
+          {...{ ...getProps(), value: '' }}
+          style={{ display: 'none' }}
+          ref={inputRef}
+        />
+        <InputStyle
+          {...{ ...getProps(), type: 'button' }}
+          onClick={() => {
+            // console.log('CLICK');
+            // console.log('click', inputRef?.current);
+            inputRef?.current?.click();
+          }}
+        />
+      </>
+    ) : (
+      <InputStyle {...getProps()} ref={inputRef} />
+    );
 
   const fullInput = props.label ? (
     <label style={props.labelParentStyle}>
       <span style={props.labelStyle}>{props.label}</span>
       {input}
     </label>
-  ):(input);
+  ) : (
+    input
+  );
 
   return (
     <>
-    {fullInput}
-    {props?.error && props?.error !== '' && props?.error !== ' ' ? (
-      <Error style={props?.errorStyle}>{props?.error}</Error>
-    ) : null}
+      {fullInput}
+      {props?.error && props?.error !== '' && props?.error !== ' ' ? (
+        <Error style={props?.errorStyle}>{props?.error}</Error>
+      ) : null}
     </>
   );
 };
