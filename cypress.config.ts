de97c63fc9bde428d69ce59af1f3a5c8c9ffe40d
Judah <judahholanda7@gmail.com>
2022-10-26 09:34:35 -0300

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'heiq2q',
   defaultCommandTimeout: 5000,
   component: {
     specPattern: "test/**/*.test.{js,jsx,ts,tsx}",
