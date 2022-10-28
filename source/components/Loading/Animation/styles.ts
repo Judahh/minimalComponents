151c453f75c58c3fc2dc18eba3cafd8aa55457ed
Judah <judahholanda7@gmail.com>
2022-10-28 12:05:21 -0300

    add hit anim

--- source/components/Loading/Animation/styles.ts
+++ source/components/Loading/Animation/styles.ts
@@ -1,6 +1,41 @@
 import styled from 'styled-components';
 import { baseConfig } from '../../../utils/config';
 
+export const Hitting = styled.div`
+  ${(props) => baseConfig(props)}
+  @keyframes hitting {
+    0% {
+      -webkit-transform: translate(0);
+      transform: translate(0);
+    }
+    20% {
+      -webkit-transform: translate(2px, -2px);
+      transform: translate(2px, -2px);
+    }
+    40% {
+      -webkit-transform: translate(2px, 2px);
+      transform: translate(2px, 2px);
+    }
+    60% {
+      -webkit-transform: translate(-2px, 2px);
+      transform: translate(-2px, 2px);
+    }
+    80% {
+      -webkit-transform: translate(-2px, -2px);
+      transform: translate(-2px, -2px);
+    }
+    100% {
+      -webkit-transform: translate(0);
+      transform: translate(0);
+    }
+  }
+  width: fit-content;
+  transform-origin: top;
+  animation: hitting
+    ${(props) => props?.theme?.animation?.hitting?.duration || 0.3}s ease-in-out
+    ${(props) => props?.theme?.animation?.hitting?.duration || 0.2}s infinite;
+`;
+
 export const Hanging = styled.div`
   ${(props) => baseConfig(props)}
   @keyframes hanging {
@@ -16,29 +51,31 @@ export const Hanging = styled.div`
   }
   width: fit-content;
   transform-origin: top;
-  animation: hanging ${(props) => props?.theme?.animation?.hanging?.duration || 2}s ease infinite;
+  animation: hanging
+    ${(props) => props?.theme?.animation?.hanging?.duration || 2}s ease infinite;
 `;
 
 export const Rowling = styled.div`
   ${(props) => baseConfig(props)}
-  @keyframes ${(props)=> props.anti ? 'anti-':''}rowling {
+  @keyframes ${(props) => (props.anti ? 'anti-' : '')}rowling {
     0% {
-      transform: rotate(${(props)=> props.anti ? '360deg':'0deg'});
+      transform: rotate(${(props) => (props.anti ? '360deg' : '0deg')});
     }
     25% {
-      transform: rotate(${(props)=> props.anti ? '270deg':'90deg'});
+      transform: rotate(${(props) => (props.anti ? '270deg' : '90deg')});
     }
     50% {
-      transform: rotate(${(props)=> props.anti ? '180deg':'180deg'});
+      transform: rotate(${(props) => (props.anti ? '180deg' : '180deg')});
     }
     75% {
-      transform: rotate${(props)=> props.anti ? '90deg':'270deg'});
+      transform: rotate${(props) => (props.anti ? '90deg' : '270deg')});
     }
     100% {
-      transform: rotate(${(props)=> props.anti ? '0deg':'360deg'});
+      transform: rotate(${(props) => (props.anti ? '0deg' : '360deg')});
     }
   }
   width: fit-content;
   transform-origin: center;
-  animation: ${(props)=> props.anti ? 'anti-':''}rowling ${(props) => props?.theme?.animation?.rowling?.duration || 2}s linear infinite;
+  animation: ${(props) => (props.anti ? 'anti-' : '')}rowling ${(props) =>
+  props?.theme?.animation?.rowling?.duration || 2}s linear infinite;
 `;
