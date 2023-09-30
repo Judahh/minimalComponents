cf5ec04ab78c17fbcab9e2b6b171a37022c286de
Judahh <judahholanda7@gmail.com>
2023-09-30 15:49:59 -0300

    fix

--- source/requests/requestError.ts
+++ source/requests/requestError.ts
@@ -2,20 +2,33 @@ export default class RequestError extends Error {
   public code: number;
   public codeText: string;
   public response;
+  public internalError;
+  public url?: string;
+  public method?: string;
 
-  constructor(code: number, codeText: string, response) {
+  constructor(
+    code: number,
+    codeText: string,
+    response,
+    internalError?,
+    url?: string,
+    method?: string
+  ) {
     codeText = codeText === 'Error' ? response.name : codeText;
-    const title = code && (codeText) ?
-      `${code} - ${codeText}` :
-      (code || codeText);
+    const title = code && codeText ? `${code} - ${codeText}` : code || codeText;
     const responseMessage = response?.message ? response.message : response;
-    const message = title && responseMessage ?
-      `${title}\n${responseMessage}` :
-      (title || responseMessage);
+    const message =
+      title && responseMessage
+        ? `${title}\n${responseMessage}`
+        : title || responseMessage;
     super(message.toString());
     this.code = code;
     this.codeText = codeText;
     this.response = response;
-    console.error(this);
+    this.internalError = internalError;
+    this.url = url;
+    this.method = method;
+
+    // console.error(this);
   }
 }
